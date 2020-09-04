9740063cf58051279d3bfdf188849f9aec977a43
1TSOP <nikita.postnov01@mail.ru>
2020-09-04 10:53:01 +0300

    SecondP

---
+++ src/ru/mirea/Main.java
@@ -0,0 +1,16 @@
+package ru.mirea;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a=5;
+        float b = 2.2f;
+        boolean  c= false;
+        String d;
+        Scanner skan = new Scanner(System.in);
+        a=skan.nextInt();
+        System.out.println(a+1);
+        b=a;
+    }
+}
