5773210a14c44721af21b2d20927815a9b14a9a1
1TSOP <nikita.postnov01@mail.ru>
2020-09-04 12:06:32 +0300

    v2

--- src/ru/mirea/Main.java
+++ src/ru/mirea/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        /////////////task1-2///////////
+        /*
         int a=5;
         float b = 2.2f;
         boolean  c= false;
@@ -11,5 +13,35 @@ public class Main {
         Scanner skan = new Scanner(System.in);
         a=skan.nextInt();
         System.out.println(a+1);
-            }
-}
+        */
+        /////////////task3///////////
+        /*
+        int [] mass = {1,2,3,4,5,7};
+        int sum = 0;
+        for (int a: mass){
+            sum+=a;
+        }
+    System.out.println("Число суммы массива равно: " +sum);
+        */
+        /////////////random///////////
+        /*
+        System.out.println(ReturnRandInt(100));
+    }
+
+    public static int ReturnRandInt(int scale) {
+        return (int)(Math.random() * scale);
+         */
+        /////////////task4///////////
+        /*
+        for (string a: args)
+        System.out.println(a);
+        */
+        /////////////task5///////////
+       /*
+        double a =0;
+        for(double n=1;n<=10;n++) {
+        a=(1/n);
+        System.out.format("%.2f%n", a);
+        */
+        }
+        }
